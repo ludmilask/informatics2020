5fa18dded6aaca335ed5789072cd240ff2a2e167
unknown <ludic_2001@mail.ru>
2020-11-05 23:54:30 +0300

    I missed 7th lab due to illnes, but I promise to do it....

---
+++ labs/lab8.py
@@ -0,0 +1,26 @@
+class ConsoleIterator:
+    def __iter__(self):
+        self.numbers = []
+        return self
+
+    def __next__(self):
+        flag = True
+        while flag:
+            if flag and len(self.numbers) == 0:
+                str = input().split()
+                for i in str:
+                    if i.isdigit():
+                        self.numbers.append(int(i))
+                    else:
+                        flag = False
+            elif len(self.numbers) != 0:
+                return self.numbers.pop(0)
+        raise StopIteration
+
+
+total_sum = 0
+
+for number in ConsoleIterator():
+    total_sum = total_sum + number
+
+print(f'Sum of entered numbers is {total_sum}')
