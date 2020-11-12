ccd26f8a1dd37bd71254dc25c325600c70b221ce
unknown <ludic_2001@mail.ru>
2020-11-12 17:48:43 +0300

    lab3 changed

--- labs/lab03/lab3.py
+++ labs/lab03/lab3.py
@@ -3,13 +3,15 @@ from os.path import isdir
 
 def tenet(input_file_path: str, output_file_path: str) -> None:
     if isdir(input_file_path):
-        raise ValueError()
+        raise ValueError(f"Expect file as input, but got directory: \"{input_file_path}\"!")
     if input_file_path == output_file_path:
-        raise ValueError()
-    r = open(input_file_path, "r")
-    w = open(output_file_path, "w")
-    for i in r.readlines():
-        w.write((i.replace("\n", ""))[::-1] + "\n")
+        raise ValueError(f"input and output files are the same")
+    file_for_reading = open(input_file_path, "r")
+    file_for_writing = open(output_file_path, "w")
+    for row in file_for_reading.readlines():
+        file_for_writing.write((row.replace("\n", ""))[::-1] + "\n")
+    file_for_reading.close()
+    file_for_writing.close()
 
 
 if __name__ == '__main__':
